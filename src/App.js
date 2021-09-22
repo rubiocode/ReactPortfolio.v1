ca98df8e656a39e4c2d1188c3994f4a0b43109b0
rubiocode <78938193+rubiocode@users.noreply.github.com>
2021-09-21 23:50:18 -0700

    added browser router

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import Home from './components/';
 import Resume from './components/Resume';
 import Portfolio from './components/Portfolio';
 import Contacts from './components/Contacts';
-import { Route } from 'react-router-dom';
+//import { Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
 
 function App() {
   return (
-    <>
-      <CssBaseline />
-      <Route exact path='/' component={Home} />
-      <Route path='/resume' component={Resume} />
-      <Route path='/portfolio' component={Portfolio} />
-      <Route path='/contacts' component={Contacts} />
-      
-    </>
+    <Router>
+      <>
+        <CssBaseline />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/resume' component={Resume} />
+          <Route path='/portfolio' component={Portfolio} />
+          <Route path='/contacts' component={Contacts} />
+        </Switch>
+      </>
+    </Router>
   );
 }
 
